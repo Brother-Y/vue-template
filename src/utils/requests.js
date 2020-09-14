1f688a916f9d977b32e3ba68b87068425c935153
Brother-Y <450028105@qq.com>
2020-09-14 10:56:37 +0800

    axios拦截器

--- src/utils/requests.js
+++ src/utils/requests.js
@@ -28,7 +28,7 @@ axios.interceptors.request.use(function (config) {
     return config;
   }, function (error) {
     // 对请求错误做些什么
-    return console.log('error');;
+    return Promise.reject(error);
   });
 
   axios.interceptors.response.use(function (response) {
